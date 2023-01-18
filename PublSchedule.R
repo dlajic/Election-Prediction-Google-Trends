b304d700f1dc06d006916b981df713183a2af7db
dlajic <dean.lajic@web.de>
2023-01-18 17:42:00 +0100

    Search Terms Data

--- PublSchedule.R
+++ PublSchedule.R
@@ -12,3 +12,9 @@ taskscheduler_create(taskname = "New", rscript = "C:/Users/deanl/Desktop/UniMann
 
 #taskscheduler_delete(taskname = "New")
  
+## Run every 24 hours
+taskscheduler_create(taskname = "New1", rscript = "C:/Users/deanl/Desktop/UniMannheim/ComSocScience/Publikation/Election-Prediction-Google-Trends/SearchTerm_selection_Datasets.R",
+                     schedule = "DAY", starttime = "14:02", modifier = 1)
+
+
+#taskscheduler_delete(taskname = "New1")
--- SearchTerm_selection.R
+++ SearchTerm_selection.R
@@ -12,7 +12,7 @@ summary(df_cat)
 ######### Search terms + period
 #Bundestagswahlen: 26.09.2021; 24.09.2017; 22.09.2013; 27.09.2009; 18.09.2005
 # Specifying vertical X intercepts for election dates
-elec_vlines <- as.Date(c("2005-09-18", "2009-09-27", "2013-09-18", "2017-09-24"))
+elec_vlines <- as.Date(c("2005-09-18", "2009-09-27", "2013-09-18", "2017-09-24", "2021-09-26"))
 
 #Approach: Look if search Querry has Peak before a election, compare the importance of the different querries
 #Attention: Here, we are looking at the impact of the querries between 2004 and 2018. Additionally, we looked for specific candidates of parties in the time intervall they were present. For that purpose Google Trends was used directly, not reported here because the amount of code would be too much.
@@ -20,7 +20,7 @@ elec_vlines <- as.Date(c("2005-09-18", "2009-09-27", "2013-09-18", "2017-09-24")
 ###############################  CDU #############################
 #result: CDU + Angela Merkel + CSU
 
-termsCDU <- gtrends(keyword=c("CDU", "CSU", "Angela Merkel", "Christlich demokratische Union"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsCDU <- gtrends(keyword=c("CDU", "CSU", "Angela Merkel", "Christlich demokratische Union"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsCDU_df <- termsCDU$interest_over_time
 termsCDU_df <- termsCDU_df %>%
@@ -38,7 +38,7 @@ ggplot(termsCDU_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 ###############################  CSU #############################
 #result: Just use CSU, no Peaks for main candidates
 
-termsCSU <- gtrends(keyword=c("CSU", "christlich soziale union", "Edmund Stoiber", "Horst Seehofer"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsCSU <- gtrends(keyword=c("CSU", "christlich soziale union", "Edmund Stoiber", "Horst Seehofer", "Alexander Dobrindt"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsCSU_df <- termsCSU$interest_over_time
 termsCSU_df <- termsCSU_df %>%
@@ -57,7 +57,7 @@ ggplot(termsCSU_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 
 #result: SPD + Candidates
 
-termsSPD <- gtrends(keyword=c("SPD", "Sozialdemokratische Partei Deutschlands", "Frank Walter Steinmeier", "Peer Steinbrück", "Martin Schulz"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsSPD <- gtrends(keyword=c("SPD", "Sozialdemokratische Partei Deutschlands", "Frank Walter Steinmeier", "Peer Steinbrück", "Martin Schulz", "Olaf Scholz"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsSPD_df <- termsSPD$interest_over_time
 termsSPD_df <- termsSPD_df %>%
@@ -75,7 +75,7 @@ ggplot(termsSPD_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 #Parteivorsitzende: Guido Westerwelle (-2011), Philipp Rösler (2011-2013), Christian Linder (2013-)
 #result: FDP + Candidate
 
-termsFDP <- gtrends(keyword=c("FDP", "Freie demokratische Partei Deutschlands", "Guido Westerwelle", "Philipp Rösler", "Christian Lindner"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsFDP <- gtrends(keyword=c("FDP", "Freie demokratische Partei Deutschlands", "Guido Westerwelle", "Philipp Rösler", "Christian Lindner"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsFDP_df <- termsFDP$interest_over_time
 termsFDP_df <- termsFDP_df %>%
@@ -93,7 +93,7 @@ ggplot(termsFDP_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 #top candidates: Joschka Fischer(2005), Jürgen Trittin + Renate Künast (2009), Jürgen Trittin + Katrin Göring Eckardt (2013), Katrin Göring-Eckardt + Cem Özdemir (2017)
 #result: Grüne (includes "Die Grünen" and "Bündnis 90 die Grünen") + top candidates
 
-termsGrün <- gtrends(keyword=c('"Grüne"',"Die Grünen", "Bündnis 90 die Grünen"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsGrün <- gtrends(keyword=c('"Grüne"',"Die Grünen", "Bündnis 90 die Grünen"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsGrün_df <- termsGrün$interest_over_time
 termsGrün_df <- termsGrün_df %>%
@@ -107,7 +107,7 @@ ggplot(termsGrün_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
   geom_vline(xintercept = elec_vlines, col= "black", linetype="dotted")
 
 
-termsGrün_Cand <- gtrends(keyword=c("Grüne","Jürgen Trittin + Renate Künast", "Jürgen Trittin + Katrin Göring Eckardt", "Katrin Göring Eckardt + Cem Özdemir"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsGrün_Cand <- gtrends(keyword=c("Grüne","Jürgen Trittin + Renate Künast", "Jürgen Trittin + Katrin Göring Eckardt", "Katrin Göring Eckardt + Cem Özdemir", "Baerbock"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 termsGrün_Cand_df <- termsGrün_Cand$interest_over_time
 termsGrün_Cand_df <- termsGrün_Cand_df %>%
   mutate(hits = as.numeric(hits), date = as.Date(date)) %>%
@@ -124,7 +124,7 @@ ggplot(termsGrün_Cand_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 #result: Linke (includes "Die Linke" and "Die Linken")
 
 #Linke vs related querries
-termsLinke <- gtrends(keyword=c("Linke", "Die Linke","Die Linken"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsLinke <- gtrends(keyword=c("Linke", "Die Linke","Die Linken"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsLinke_df <- termsLinke$interest_over_time
 termsLinke_df <- termsLinke_df %>%
@@ -141,7 +141,7 @@ ggplot(termsLinke_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 
 #Spitzenkandidaten
 #for election 2013: Sarah Wagenknecht and Gregor Gysi (Most important ones of the 8 candidates due to Google Trends)
-termsLinkeSK <- gtrends(keyword=c("Gregor Gysi", "Gregor Gysi + Sarah Wagenknecht", "Dietmar Bartsch + Sarah Wagenknecht"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsLinkeSK <- gtrends(keyword=c("Gregor Gysi", "Gregor Gysi + Sarah Wagenknecht", "Dietmar Bartsch + Sarah Wagenknecht", "Janine Wissler + Dietmar Bartsch"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsLinke_dfSK <- termsLinkeSK$interest_over_time
 termsLinke_dfSK <- termsLinke_dfSK %>%
@@ -176,7 +176,7 @@ ggplot(termsLinke2_df, aes(x=date, y=hits, group=keyword, col=keyword)) +
 #Spitzenkandidaten:  Bernd Lucke (2013),Alice Weidel + Alexander Gauland (2017),Alice Weidel + Tino Chrupalla (2021)
 #result: AFD + candidates
 
-termsAFD <- gtrends(keyword=c("AFD", "Alternative für Deutschland", "Bernd Lucke", "Alice Weidel + Alexander Gauland"), geo= "DE" , category=19, time = "2004-01-01 2018-01-01", gprop="web")
+termsAFD <- gtrends(keyword=c("AFD", "Alternative für Deutschland", "Bernd Lucke", "Alice Weidel + Alexander Gauland", "Alice Widel + Tino Chrupalla"), geo= "DE" , category=19, time = "2004-01-01 2021-12-31", gprop="web")
 
 termsAFD_df <- termsAFD$interest_over_time
 termsAFD_df <- termsAFD_df %>%
